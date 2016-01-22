318c7d78ebd918fc2a9ba80c0fd1c800e15b71a7
Lê Tuấn Kiệt <letuankiet146@gmail.com>
2016-01-22 09:39:48 +0700

    preference table: Account Customer FormOder

--- Server/spr-data/src/main/java/com/spr/model/Account.java
+++ Server/spr-data/src/main/java/com/spr/model/Account.java
@@ -1,5 +1,101 @@
 package com.spr.model;
 
-public class Account {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+/**
+ * 
+ * @author TuanKiet
+ *
+ */
+@Entity
+@Table(name="account")
+public class Account {
+	@Id
+	@Column(name="account_id")
+	private Integer accountId;
+	
+	@Column(name="account_user")
+	private String accountUser;
+	
+	@Column(name="account_password")
+	private String accountPassword;
+	
+	@Column(name="account_type")
+	private Integer accountType;
+	
+	@Column(name="account_lock")
+	private Integer accountLock;
+
+	/**
+	 * @return the accountId
+	 */
+	public Integer getAccountId() {
+		return accountId;
+	}
+
+	/**
+	 * @param accountId the accountId to set
+	 */
+	public void setAccountId(Integer accountId) {
+		this.accountId = accountId;
+	}
+
+	/**
+	 * @return the accountUser
+	 */
+	public String getAccountUser() {
+		return accountUser;
+	}
+
+	/**
+	 * @param accountUser the accountUser to set
+	 */
+	public void setAccountUser(String accountUser) {
+		this.accountUser = accountUser;
+	}
+
+	/**
+	 * @return the accountPassword
+	 */
+	public String getAccountPassword() {
+		return accountPassword;
+	}
+
+	/**
+	 * @param accountPassword the accountPassword to set
+	 */
+	public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+	}
+
+	/**
+	 * @return the accountType
+	 */
+	public Integer getAccountType() {
+		return accountType;
+	}
+
+	/**
+	 * @param accountType the accountType to set
+	 */
+	public void setAccountType(Integer accountType) {
+		this.accountType = accountType;
+	}
+
+	/**
+	 * @return the accountLock
+	 */
+	public Integer getAccountLock() {
+		return accountLock;
+	}
+
+	/**
+	 * @param accountLock the accountLock to set
+	 */
+	public void setAccountLock(Integer accountLock) {
+		this.accountLock = accountLock;
+	}
 }
--- Server/spr-data/src/main/java/com/spr/model/FormOrder.java
+++ Server/spr-data/src/main/java/com/spr/model/FormOrder.java
@@ -1,5 +1,33 @@
 package com.spr.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author TuanKiet
+ *
+ */
+@Entity
+@Table(name = "form_order")
 public class FormOrder {
+	@Id
+	@Column(name = "form_order_id")
+	private Integer formOrderId;
+
+	@ManyToOne
+	@JoinColumn(name = "form_order_tour_id")
+	private Tour formOderTourId;
+	
+	@ManyToOne
+	@JoinColumn(name = "form_order_customer_id")
+	private Customer formOderCustomerId;
+
+	@Column(name = "form_order_date")
+	private Integer formOderDate;
 
 }
